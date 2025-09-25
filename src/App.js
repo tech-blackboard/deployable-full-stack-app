65a91920e675abeddaa87780585b630b03037e0e
Pooja Thatikonda <poojathatikonda888@gmail.com>
2025-09-25 12:53:34 +0530

    Tasklist view UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-// import { Routes } from 'react-router-dom';
-// import { Route } from 'react-router-dom';
-// import Register from './Componenets.js/Register';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Register from './Componenets.js/Register';
 import { ToastContainer } from "react-toastify";
 import ProjectView from './Componenets.js/ProjectView';
-// import Login from './Componenets.js/Login';
+import Login from './Componenets.js/Login';
 import DashBoard from './Componenets.js/DashBoard';
+import TaskListView from './Componenets.js/TaskListView';
 import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
@@ -23,14 +24,16 @@ function App() {
         pauseOnHover
         theme="colored"
       />
-     <DashBoard/>
-     <ProjectView/>
-{/* 
-      <Routes>
+    
+     {/* <ProjectView/> */}
+     <TaskListView/>
+
+      {/* <Routes>
          <Route path='/' element={ <Login/>}/>
         <Route path='/Login' element={ <Login/>}/>
         <Route path='/Register' element={<Register/>}/>
-
+        <Route path='/Dashboard' element={<DashBoard/>}/>
+ 
            
         
       </Routes> */}
