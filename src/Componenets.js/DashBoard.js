d68b8c4a1fc4e33addffa70a0460567d72c9d57f
Pooja Thatikonda <poojathatikonda888@gmail.com>
2025-10-09 19:40:57 +0530

    dashboard user interface

--- src/Componenets.js/DashBoard.js
+++ src/Componenets.js/DashBoard.js
@@ -1,10 +1,9 @@
-// import { Link } from "react-router-dom";
 import './Dashboard.css';
 
 export default function DashBoard(){
     return(
-        <div id="main">
-            <div id="nav">
+        <div className="main">
+            <div className="nav">
                  <h2>TaskManager</h2>
             <nav>
                 <a href='/DashBoard'>DashBoard</a>
@@ -16,35 +15,37 @@ export default function DashBoard(){
             </div>
          
                 <h3>Welcome back, Pooja!👋</h3>
-                <p id="headp">Here's What's happening with your project </p>
-             <div id="body">
+                <p className="headp">Here's What's happening with your project </p>
+
+             <div className="body">
                 
-                <div><p style={{color:" rgba(120, 120, 254, 1)" ,fontSize:"20px",fontWeight:"bolder",fontFamily:"Times New Roman"}}>5</p>Active Projects</div>
+                <div><p style={{color:" rgba(120, 120, 254, 1)" ,fontSize:"30px",fontWeight:"bolder",fontFamily:"Times New Roman"}}>5</p>Active Projects</div>
                 
-                <div><p style={{color:" rgba(120, 120, 254, 1)" ,fontWeight:"bolder",fontSize:"20px",fontFamily:"Times New Roman"}}>23</p>Total Tasks</div>
+                <div><p style={{color:" rgba(120, 120, 254, 1)" ,fontWeight:"bolder",fontSize:"30px",fontFamily:"Times New Roman"}}>23</p>Total Tasks</div>
                 
-                <div> <p style={{color:" rgba(120, 120, 254, 1)" ,fontWeight:"bolder",fontSize:"20px",fontFamily:"Times New Roman"}}>8</p>Completed</div>
+                <div> <p style={{color:" rgba(120, 120, 254, 1)" ,fontWeight:"bolder",fontSize:"30px",fontFamily:"Times New Roman"}}>8</p>Completed</div>
                 
-                <div><p style={{color:" rgba(120, 120, 254, 1)" ,fontSize:"20px",fontWeight:"bolder",fontFamily:"Times New Roman"}}>3</p>Overdue</div>
+                <div><p style={{color:" rgba(120, 120, 254, 1)" ,fontSize:"30px",fontWeight:"bolder",fontFamily:"Times New Roman"}}>3</p>Overdue</div>
                 
             </div>
             <div className="recentp">
-                <p id="recept">Recent Projects</p>
-                <button id="newp">New Project</button>
+                <p className="recent">Recent Projects</p>
+                <button className="newproject">New Project</button>
             </div>
-            <div id="web">
-             <h4>E-commerce Website</h4>
-             <p>12 tasks • 3 completed</p>
-             <div id="progress-container">
-               <div id="progress-bar"  ></div>
-             </div>
+            <div className="web">
+               <h4>E-commerce Website</h4>
+               <p>12 tasks • 3 completed</p>
+                 <div className="progress-container">
+                    <div className="progress-bar"  ></div>
+                  </div>
             </div>
-            <div id="web">
-             <h4>Mobile App Redesign</h4>
-             <p>8 tasks • 6 completed</p>
-             <div id="progress-container">
-               <div id="progress-bar"></div>
-             </div>
+
+            <div className="web">
+               <h4>Mobile App Redesign</h4>
+               <p>8 tasks • 6 completed</p>
+                 <div className="progress-container">
+                    <div className="progress-bar"></div>
+                  </div>
             </div>
         </div>
     )
